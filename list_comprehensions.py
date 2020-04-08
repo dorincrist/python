0e4e6d3ef5787363ab8c6b003708d6b83a1883e0
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-04-08 11:53:00 +0200

    list comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+colors = ['red', 'green', 'orange', 'blue']
+uppercase_colors = []
+
+for color in colors:
+    uppercase_colors.append(color.upper())
+
+print(uppercase_colors)
