020354875376a94ee41e63efb38f6932013a248f
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-04-08 11:30:31 +0200

    loop execution with 'break'

---
+++ loop_break.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+colours = ['blue', 'red', 'green', 'orange']
+for color in colours:
+   if color == 'blue':
+      continue
+   elif color == 'green':
+      break
+   print(color)
--- loop_continue.py
+++ loop_continue.py
@@ -3,7 +3,7 @@
 count = 1
 while count <= 20:
    if count % 2 == 0:
-         count += 1
+         #count += 1
          continue
    
    print(f"We are couting odd numbers: {count}")
