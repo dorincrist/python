6d5c9aea8e69fffa134e801d906e98fc0651e64c
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-04-08 11:24:18 +0200

    loop execution with 'continue'

---
+++ loop_continue.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+
+count = 1
+while count <= 20:
+   if count % 2 == 0:
+         count += 1
+         continue
+   
+   print(f"We are couting odd numbers: {count}")
+   count += 1
