f0f851916537e9c89234d6da028159ea50cf5072
Dorinel Cristescu <dorinelcristescu@gmail.com>
2020-04-15 17:28:51 +0200

    read input as a float

---
+++ float_input.py
@@ -0,0 +1,9 @@
+a = float(input("Please enter a float value?")) # input a float value for variable a here
+b = float(input("Please enter a second float value?")) # input a float value for variable b here
+
+print(a + b) # output the result of addition here
+print(float(a - b))# output the result of subtraction here
+print(float(a * b))# output the result of multiplication here
+print(float(a / b)) # output the result of division here
+
+print("\nThat's all, folks!")
